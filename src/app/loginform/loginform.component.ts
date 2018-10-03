fdaa3b62dd7d8e7adbc4891c702fa2ebf383a760
rochdi1 <rochdi.abdou3@gmail.com>
2018-10-03 22:24:32 +0200

    navigate

--- src/app/loginform/loginform.component.ts
+++ src/app/loginform/loginform.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LoginService } from 'src/app/Services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'loginform',
@@ -12,11 +13,16 @@ export class LoginformComponent  {
   lg_password: string;
   status:boolean;
 
-  constructor(private mylogin: LoginService) { }
+  constructor(private mylogin: LoginService, private myrouter: Router) { }
 
   login() {
     var result = this.mylogin.login(this.lg_username, this.lg_password);
     this.status = result;
+    if (this.status) {
+      this.myrouter.navigate(['/first']);
+    } else {
+      this.myrouter.navigate(['/other']);
+    }
     console.log(result);
   }
 
